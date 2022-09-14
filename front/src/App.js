7d7a55f003432afc34d719e047d58a530cb512cf
Michael Costello <mcostello91@gmail.com>
2022-09-13 17:17:43 -0700

    Edit routing

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 // import things from react-router-dom
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 // import pages
 import Login from "./Pages/Login";
@@ -10,12 +10,10 @@ import Feed from "./Pages/Feed";
 
 function App() {
   return (
-    <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/feed" element={<Feed />} />
         </Routes>
-    </BrowserRouter>
   );
 }
 
