1a3da60d1a8b1f5c1ea3023726fa85b8bede9a65
Michael Costello <mcostello91@gmail.com>
2022-09-13 17:09:32 -0700

    Implement hash router

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import "./index.css";
 
@@ -54,5 +55,7 @@ console.log("4. Switching accounts using the 'Switch' button in the info-panel (
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <HashRouter>
     <App />
+  </HashRouter>
 );
